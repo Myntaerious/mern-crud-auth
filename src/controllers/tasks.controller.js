4dba675d40e46f0b41a994cd272afabf49d7582b
Myntaerious <mikanifati@gmail.com>
2024-06-19 17:04:09 -0400

    backend completed

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,7 +1,9 @@
 import Task from '../models/task.model.js'
 
 export const getTasks = async (req,res) =>{
-    const tasks = await Task.find();
+    const tasks = await Task.find({
+        user: req.user.id
+    }).populate("user");
     res.json(tasks); 
 }
 
@@ -10,23 +12,26 @@ export const createTask = async (req,res) =>{
     const newTask = new Task({
         title,
         description,
-        date
+        date,
+        user: req.user.id,
     })
     const savedTask = await newTask.save();
     res.json(savedTask);
 }
 
 export const getTask = async (req,res) =>{
-    const task = await Task.findById(req.params.id)
+    const task = await Task.findById(req.params.id).populate("user")
     if(!task) return res.status(404).json({message: "task not found"});
     res.json(task);
 }
 
-export const updateTask = async (req,res) =>{}
+export const deleteTask = async (req,res) =>{
     const task = await Task.findByIdAndDelete(req.params.id)
     if(!task) return res.status(404).json({message: "task not found"});
-    res.json(task);
-export const deleteTask = async (req,res) =>{
+    return res.sendStatus(204);
+}
+
+export const updateTask = async (req,res) =>{
     const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
         new: true
     })
--- src/models/task.model.js
+++ src/models/task.model.js
@@ -13,6 +13,11 @@ date: {
     type: Date,
     default: Date.now,
 },
+user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+},
 },
 {
     timestamps: true
